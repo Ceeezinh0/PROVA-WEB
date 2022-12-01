34a8a1473750db770fe46d0f45b22ec063d07c28
Cristiano Marques Silva <cristimarsilva.cris@gmail.com>
2022-12-01 15:25:24 -0300

    PROVA DE PROGRAMAÇÃO WEB
    30/11/2022

--- JAVA.js
+++ JAVA.js
@@ -3,5 +3,5 @@ var nome = prompt("Digite o seu nome: ");
 alert ('Seja bem vindo ' +nome+ ', conclua o formulario para proseguir')
 
 function enviar(){
-alert("Obrigado por completar o cadastro.")
+    alert("Obrigado por completar o cadastro.")
 }

46a9137e4287974867fcd7f33a4d20897f04b124
Cristiano Marques Silva <cristimarsilva.cris@gmail.com>
2022-12-01 15:22:22 -0300

    Initial commit

---
+++ JAVA.js
@@ -0,0 +1,7 @@
+var nome = prompt("Digite o seu nome: ");
+
+alert ('Seja bem vindo ' +nome+ ', conclua o formulario para proseguir')
+
+function enviar(){
+alert("Obrigado por completar o cadastro.")
+}
